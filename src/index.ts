2ed02e85a3a40815f8f97389d1b31e41c18bbc59
Clovis de Villars <clovis.devillars@gmail.com>
2021-02-15 00:35:28 +0100

    Add index file
    Index file export all lib modules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello world')
+import ArchiveFactory from './classes/ArchiveFactory'
+import Archive from './classes/Archive/Archive'
+import Standardizer from './classes/Standardizer/Standardizer'
+import ParsingUtils from './modules/ParsingUtils'
+
+export default {
+  ArchiveFactory,
+  Archive,
+  Standardizer,
+  ParsingUtils,
+}
+
+// Named export classes
+export { default as ArchiveFactory } from './classes/ArchiveFactory'
+export { default as Archive } from './classes/Archive/Archive'
+export { default as Standardizer } from './classes/Standardizer/Standardizer'
+
+// Named export modules
+export { default as ParsingUtils } from './modules/ParsingUtils'
