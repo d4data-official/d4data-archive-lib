e6d2129bee801eecd8040d40822d1f8a6a3ba68b
Pedro <pedrosf1000@gmail.com>
2021-02-27 19:24:28 +0100

    Add  ListFiles parsing function

--- src/modules/Parsing/listFiles.ts
+++ src/modules/Parsing/listFiles.ts
@@ -2,9 +2,32 @@
  * List all files recursively in given directory path and return absolute path list
  * Throw error if can't access directory
  */
+import path from 'path'
+import { readdir, stat } from 'fs/promises';
 import { FilterOptions } from '../../types/Parsing'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default async function listFiles(dirPath: string, options?: FilterOptions): Promise<Array<string>> {
-  return Promise.reject(new Error('Not implemented'))
+  return getFiles(dirPath, [], options)
+}
+
+async function getFiles(dir: string, files_: Array<string> = [], options?: FilterOptions) {
+  const files = await readdir(dir);
+  for await (const file of files) {
+    const name = path.resolve(dir, file);
+    if ((await stat(name)).isDirectory()) {
+      await getFiles(name, files_, options);
+    } else {
+      const extension = name.split('.').pop()?.toLowerCase() ?? ''
+      if (options?.extensionWhitelist && !options?.extensionWhitelist?.includes(extension)) {
+        // eslint-disable-next-line no-continue
+        continue
+      }
+      if (options?.extensionBlacklist && options?.extensionBlacklist?.includes(extension)) {
+        // eslint-disable-next-line no-continue
+        continue
+      }
+      files_.push(name);
+    }
+  }
+  return files_;
 }
