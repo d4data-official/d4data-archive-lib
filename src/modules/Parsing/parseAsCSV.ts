e2025c0e89b89e13c3629ef1a96752948ddca93f
Pedro <pedrosf1000@gmail.com>
2021-02-28 02:17:59 +0100

    Add column option to parsing CSV

--- src/modules/Parsing/parseAsCSV.ts
+++ src/modules/Parsing/parseAsCSV.ts
@@ -2,12 +2,16 @@ import papa from 'papaparse'
 import fs from 'fs'
 import { PaginationOptions, ParsingOptions } from '../../types/Parsing'
 
+export interface OptionsCSV {
+  columns?: Array<string>
+}
+
 /**
  * Parse CSV file from given path
  * Throw error if can't access file or file format is invalid
  */
 export default async function parseAsCSV<T = unknown>(filePath: string,
-  options?: ParsingOptions & PaginationOptions): Promise<Array<T>> {
+  options?: ParsingOptions & PaginationOptions & OptionsCSV): Promise<Array<T>> {
   const stream = fs.createReadStream(filePath);
   const content: Array<T> = [];
   const items = options?.pagination?.items ?? 0
@@ -15,6 +19,7 @@ export default async function parseAsCSV<T = unknown>(filePath: string,
 
   return new Promise((resolve) => {
     papa.parse(stream, {
+      header: !options?.columns,
       step(row, parser) {
         if (items === 0) {
           // @ts-ignore
@@ -35,4 +40,19 @@ export default async function parseAsCSV<T = unknown>(filePath: string,
       },
     });
   })
+    .then((data) => {
+      if (!options?.columns) {
+        return data
+      }
+      // @ts-ignore
+      return data.map((item) => {
+        const obj = {}
+        for (let i = 0; i < (options?.columns?.length ?? 0); i += 1) {
+          const colName = options?.columns![i]
+          // @ts-ignore
+          obj[colName] = item[i]
+        }
+        return obj
+      })
+    })
 }
