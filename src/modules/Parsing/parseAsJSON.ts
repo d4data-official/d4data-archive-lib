e192067e256f771edc0a117ebe836ba3bf6da6fa
Clovis de Villars <clovis.devillars@gmail.com>
2021-02-28 16:48:21 +0100

    Update parsing utilities doc comments

--- src/modules/Parsing/parseAsJSON.ts
+++ src/modules/Parsing/parseAsJSON.ts
@@ -2,7 +2,7 @@ import { ParsingOptions } from '../../types/Parsing'
 
 /**
  * Parse JSON file from given path
- * Throw error if can't access file or file format is invalid
+ * Throw error if can't access file or if parsing fail
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default async function parseAsJSON<T = any>(filePath: string, options?: ParsingOptions): Promise<T> {
