470a0a1bc2f9d0db48afa8173c93d40c39cf2625
Pedro <pedrosf1000@gmail.com>
2021-02-28 01:46:56 +0100

    Add CSV parsing feature

--- src/modules/Parsing/parseAsCSV.ts
+++ src/modules/Parsing/parseAsCSV.ts
@@ -1,14 +1,38 @@
+import papa from 'papaparse'
+import fs from 'fs'
 import { PaginationOptions, ParsingOptions } from '../../types/Parsing'
 
 /**
  * Parse CSV file from given path
  * Throw error if can't access file or file format is invalid
  */
-export default async function parseAsCSV<T = unknown>(
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  filePath: string,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  options?: ParsingOptions & PaginationOptions,
-): Promise<Array<T>> {
-  return Promise.reject(new Error('Not implemented'))
+export default async function parseAsCSV<T = unknown>(filePath: string,
+  options?: ParsingOptions & PaginationOptions): Promise<Array<T>> {
+  const stream = fs.createReadStream(filePath);
+  const content: Array<T> = [];
+  const items = options?.pagination?.items ?? 0
+  const offset = options?.pagination?.offset ?? 0
+
+  return new Promise((resolve) => {
+    papa.parse(stream, {
+      step(row, parser) {
+        if (items === 0) {
+          // @ts-ignore
+          content.push(row.data)
+          return;
+        }
+        if (offset + items === row.meta.cursor) {
+          parser.abort()
+          return
+        }
+        if (offset >= row.meta.cursor) {
+          // @ts-ignore
+          content.push(row.data);
+        }
+      },
+      complete() {
+        resolve(content)
+      },
+    });
+  })
 }
