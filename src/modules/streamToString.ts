6d0d5c4649f7603033e1784fc4b0516054477bd2
Clovis de Villars <clovis.devillars@gmail.com>
2021-03-03 19:43:49 +0100

    Add missing comment for streamToString function

--- src/modules/streamToString.ts
+++ src/modules/streamToString.ts
@@ -1,5 +1,9 @@
 import { Stream } from 'stream'
 
+/**
+ * Read and accumulate stream data chunks and wait for stream ending to return full data
+ * WARNING : This function will put all stream data into RAM, use with caution!
+ */
 export default async function streamToString(stream: Stream): Promise<string> {
   let data = ''
 
