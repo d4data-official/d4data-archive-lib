fc2f84b162c93ec26ddfaa18976bac22068a7f67
Clovis de Villars <clovis.devillars@gmail.com>
2021-03-13 15:21:11 +0100

    Fix Standardizer plugins getters

--- src/classes/Standardizer/Standardizer.ts
+++ src/classes/Standardizer/Standardizer.ts
@@ -130,7 +130,10 @@ export default abstract class Standardizer {
    */
   static getPlugins(): Promise<Array<typeof Standardizer>> {
     return fs.promises.readdir(path.resolve(__dirname, 'services'))
-      .then(dirContent => dirContent.map(service => import(path.resolve(__dirname, 'services', service))))
+      .then(dirContent => dirContent.map(
+        service => import(path.resolve(__dirname, 'services', service))
+          .then(importedModule => importedModule.default),
+      ))
       .then(promiseArr => Promise.all(promiseArr))
   }
 
@@ -140,7 +143,7 @@ export default abstract class Standardizer {
   static getPluginsSync(): Array<typeof Standardizer> {
     return fs.readdirSync(path.resolve(__dirname, 'services')).map(
       // eslint-disable-next-line import/no-dynamic-require,global-require
-      service => require(path.resolve(__dirname, 'services', service)),
+      service => require(path.resolve(__dirname, 'services', service)).default,
     )
   }
 }
