434a588ec4a635966c1f9c9473c7ab1b5f3c0945
Clovis de Villars <clovis.devillars@gmail.com>
2021-03-15 12:41:38 +0100

    Implement service identifying methods for all current Archive plugins
    Implement service identifying methods for Facebook/Reddit/Discord/Google Archive plugins

--- src/classes/Archive/plugins/Reddit.ts
+++ src/classes/Archive/plugins/Reddit.ts
@@ -2,10 +2,16 @@ import Archive from '../Archive'
 import Services from '../../../types/Services'
 import Standardizer from '../../Standardizer/Standardizer'
 import RedditStandardizer from '../../Standardizer/plugins/Reddit/Reddit'
+import archiveFileExist from '../../../modules/archiveFileExist'
 
 export default class Reddit extends Archive {
   identifyService(): Promise<boolean> {
-    throw new Error('Not implemented')
+    return archiveFileExist(this.path, [
+      'reddit_gold_information.csv',
+      'subscribed_subreddits.csv',
+      'approved_submitter_subreddits.csv',
+    ])
+      .then(results => results.every(item => item))
   }
 
   get service(): Services {
