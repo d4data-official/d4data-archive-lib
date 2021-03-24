7dca42635d12e7c6765fd09f632646a743e0b748
Nathan Bonnot <nathan.bonnot@epitech.eu>
2021-03-24 18:12:32 +0100

    Reddit getApi

--- src/classes/Standardizer/plugins/Reddit/Reddit.ts
+++ src/classes/Standardizer/plugins/Reddit/Reddit.ts
@@ -1,5 +1,7 @@
-import Standardizer from '../../Standardizer'
+import Standardizer from 'classes/Standardizer/Standardizer'
 import Services from '../../../../types/Services'
+import { GetterOptions } from 'types/standardizer/Standardizer'
+import API from 'types/schemas/API'
 
 export default class Reddit extends Standardizer {
   get service(): Services {
@@ -13,4 +15,15 @@ export default class Reddit extends Standardizer {
   get subStandardizers(): Array<Standardizer> {
     return []
   }
+
+  async getAPIs(options?: GetterOptions): Promise<Array<API> | null> {
+    const APIRawData = await this.parser.parseAsCSV(
+      'twitter.csv',
+      options?.parsingOptions,
+    )
+    return APIRawData.map(API => ({
+      name: API.username,
+    }))
+  }
+
 }
