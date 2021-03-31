3b294cdf8efd663bf4b4d321c7356d3660f13478
Nathan Bonnot <nathan.bonnot@epitech.eu>
2021-03-31 14:12:16 +0200

    Reddit: GetReactions

---
+++ src/classes/Standardizer/plugins/Reddit/getters/getReacted.ts
@@ -0,0 +1,22 @@
+import { Reacted } from 'types/schemas'
+import Reddit from '../Reddit'
+
+Reddit.prototype.getReacted = async function getReacted(options) {
+  const ReactsPostRawData = await this.parser.parseAsCSV(
+    'post_votes.csv',
+    options?.parsingOptions,
+  )
+  const ReactsCommentRawData = await this.parser.parseAsCSV(
+    'comment_votes.csv',
+    options?.parsingOptions,
+  )
+  const FinalRawData = ReactsPostRawData.concat(ReactsCommentRawData)
+  const reacteds = FinalRawData.map((reaction):Reacted => ({
+    type: 'post',
+    entity: reaction.permalink,
+  }))
+  return {
+    data: reacteds,
+    parsedFiles: ['friends.csv'],
+  }
+}
