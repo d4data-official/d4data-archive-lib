53ab5824b906e78da4454d4d706a80c6151ad095
Nathan Bonnot <nathan.bonnot@epitech.eu>
2021-03-31 13:56:10 +0200

    Reddit: GetReactions

--- src/classes/Standardizer/plugins/Reddit/Reddit.ts
+++ src/classes/Standardizer/plugins/Reddit/Reddit.ts
@@ -1,8 +1,6 @@
 import path from 'path'
 import Standardizer, { EXTERNAL_GETTERS_DIR } from '../../Standardizer'
 import Services from '../../../../types/Services'
-import { GetterOptions } from '../../../../types/standardizer/Standardizer'
-import GetterReturn from '../../../../types/standardizer/GetterReturn'
 
 export default class Reddit extends Standardizer {
   get service(): Services {
@@ -15,26 +13,6 @@ export default class Reddit extends Standardizer {
 
   get subStandardizers(): Array<Standardizer> {
     return []
-  }
-
-  async getReacted(options?: GetterOptions): GetterReturn<Array<Reacted>> {
-    const ReactsPostRawData = await this.parser.parseAsCSV(
-      'post_votes.csv',
-      options?.parsingOptions,
-    )
-    const ReactsCommentRawData = await this.parser.parseAsCSV(
-      'comment_votes.csv',
-      options?.parsingOptions,
-    )
-    const FinalRawData = ReactsPostRawData.concat(ReactsCommentRawData)
-    const stck = FinalRawData.map(friend => ({
-      type: 'post',
-      entity: {},
-    }))
-    return {
-      data: stck,
-      parsedFiles: ['friends.csv'],
-    }
   }
 }
 
