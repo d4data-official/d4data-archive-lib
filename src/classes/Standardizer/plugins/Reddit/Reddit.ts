3567280c6a6ff768984ea6547ff8a97eb1144b42
Nathan Bonnot <nathan.bonnot@epitech.eu>
2021-03-31 12:41:40 +0200

    Reddit: GetFriends

--- src/classes/Standardizer/plugins/Reddit/Reddit.ts
+++ src/classes/Standardizer/plugins/Reddit/Reddit.ts
@@ -1,4 +1,7 @@
 import Standardizer from '../../Standardizer'
+import { Contact } from '../../../../types/schemas'
+import { GetterOptions } from '../../../../types/standardizer/Standardizer'
+import GetterReturn from '../../../../types/standardizer/GetterReturn'
 import Services from '../../../../types/Services'
 
 export default class Reddit extends Standardizer {
@@ -12,5 +15,19 @@ export default class Reddit extends Standardizer {
 
   get subStandardizers(): Array<Standardizer> {
     return []
+  }
+
+  async getFriends(options?: GetterOptions): GetterReturn<Array<Contact>> {
+    const FriendsRawData = await this.parser.parseAsCSV(
+      'friend.csv',
+      options?.parsingOptions,
+    )
+    const stck = FriendsRawData.map(friend => ({
+      displayName: friend.username,
+    }))
+    return {
+      data: stck,
+      parsedFiles: ['friend.csv'],
+    }
   }
 }
