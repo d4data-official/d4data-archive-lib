efb49419d7266fffa2ccae16cda2bcb14c92e63f
Clovis de Villars <clovis.devillars@gmail.com>
2021-04-01 02:41:58 +0200

    fix(types): fix bad typing in Google archive plugin

--- src/classes/Archive/plugins/Google.ts
+++ src/classes/Archive/plugins/Google.ts
@@ -18,7 +18,7 @@ export default class Google extends Archive {
     }
 
     const dom = await parseAsHTML(await Pipeline.fromArchive(this.path, 'Takeout/archive_browser.html'))
-    const htmlTitle: string | undefined = dom.window.document.querySelector('title')?.textContent
+    const htmlTitle = dom.window.document.querySelector('title')?.textContent
 
     return htmlTitle?.endsWith('Google') ?? false
   }
