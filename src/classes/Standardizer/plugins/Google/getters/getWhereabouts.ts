8b7a347fab4f171d142db615204b4c6f21a8ba37
Jessy SOBREIRO <jessy.sobreiro@epitech.eu>
2021-04-05 21:00:07 +0200

    Add getWhereabouts()

---
+++ src/classes/Standardizer/plugins/Google/getters/getWhereabouts.ts
@@ -0,0 +1,32 @@
+import Google from '../Google'
+import { Whereabout } from '../../../../../types/schemas';
+
+// eslint-disable-next-line
+const WHEREABOUTS_FILE = 'Takeout/Historique\ des\ Positions/Historique\ des\ Positions.json'
+
+interface GoogleWhereabouts {
+  locations: Array<{
+    timestampMs: string,
+    latitudeE7: number,
+    longitudeE7: number
+  }>
+}
+
+Google.prototype.getWhereabouts = async function getWhereabouts(options) {
+  const whereaboutList = await this.parser.parseAsJSON<GoogleWhereabouts>(WHEREABOUTS_FILE, options?.parsingOptions)
+
+  const whereabouts : Array<Whereabout> = whereaboutList.locations.map((whereabout) => ({
+    recordDate: new Date(whereabout.timestampMs),
+    location: {
+      absolutePosition: {
+        latitude: whereabout.latitudeE7,
+        longitude: whereabout.longitudeE7,
+      },
+    },
+  }))
+
+  return {
+    data: whereabouts,
+    parsedFiles: [WHEREABOUTS_FILE],
+  }
+}
