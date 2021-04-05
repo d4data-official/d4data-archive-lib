da72a2537f12339af2119ca1fee11407a3642ede
Clovis de Villars <clovis.devillars@gmail.com>
2021-04-05 22:52:23 +0200

    feat(parsing): add filter and transform callback to parseAsJSONL

--- src/modules/Parsing/parseAsJSONL.ts
+++ src/modules/Parsing/parseAsJSONL.ts
@@ -2,7 +2,14 @@ import readline from 'readline'
 import { PaginationOptions, ParsingOptions } from '../../types/Parsing'
 import Pipeline from '../../classes/Pipeline'
 
-export type OptionsParseAsJSONL = ParsingOptions & PaginationOptions
+export interface OptionsJSONL {
+  // Filter unparsed lines (JSON string)
+  filter?: (unparsedLine: string) => boolean
+  // Transform each parsed item
+  transform?: (item: any) => any
+}
+
+export type OptionsParseAsJSONL = ParsingOptions & PaginationOptions & OptionsJSONL
 
 /**
  * Parse given Pipeline result stream as JSONL format
@@ -11,28 +18,39 @@ export default async function parseAsJSONL<T = any>(
   pipeline: Pipeline,
   options?: OptionsParseAsJSONL,
 ): Promise<Array<T>> {
-  const content: Array<T> = []
   let i = 0
+  const offset = options?.pagination?.offset ?? 0
+  const itemLimit = options?.pagination?.items ?? Infinity
+  const content: Array<T> = []
   const stream = pipeline.run()
   const rl = readline.createInterface({
     input: stream,
-  });
+  })
 
   rl.on('line', (line) => {
-    if (i < (options?.pagination?.offset ?? 0)) {
+    if (!(options?.filter?.(line) ?? true)) {
+      return
+    }
+
+    if (i < offset) {
       i += 1
       return
     }
-    if (i >= (options?.pagination?.offset ?? 0) + (options?.pagination?.items ?? Infinity)) {
+
+    if (content.length >= itemLimit) {
       stream.destroy()
       rl.close()
       return
     }
-    content.push(JSON.parse(line))
-    i += 1
-  });
 
-  return new Promise <Array<T>>((resolve, reject) => {
+    const parsedItem = JSON.parse(line)
+
+    content.push(options?.transform?.(parsedItem) ?? parsedItem)
+
+    i += 1
+  })
+
+  return new Promise<Array<T>>((resolve, reject) => {
     rl.on('close', () => resolve(content))
     stream.on('error', (e) => reject(e))
   })
