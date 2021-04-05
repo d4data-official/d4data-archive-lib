f2245313adadd704220fe02924714810f0e2b628
Nathan Bonnot <nathan.bonnot@epitech.eu>
2021-04-05 16:31:18 +0200

    Added await Promise

--- src/classes/Standardizer/plugins/Google/getters/getChats.ts
+++ src/classes/Standardizer/plugins/Google/getters/getChats.ts
@@ -5,16 +5,16 @@ const ACCOUNT_CHAT_FILE = 'Hangouts/Hangouts.json'
 
 Google.prototype.getChats = async function getChats(options) {
   const rawChats = await this.parser.parseAsJSON(ACCOUNT_CHAT_FILE, options?.parsingOptions)
-  const chatConversation: Array<Chat> = rawChats.conversations.map((conv: any) => ({
+  const chatConversation: Array<Chat> = await Promise.all(rawChats.conversations.map((conv: any) => ({
     id: conv.conversation.conversation_id,
     participants: conv.conversation.conversation.participant_data.map((participant: any) => participant.fallback_name),
     title: 'None',
-  }))
-  const chatEvent: Array<Chat> = rawChats.conversations.map((event: any) => ({
+  })))
+  const chatEvent: Array<Chat> = await Promise.all(rawChats.conversations.map((event: any) => ({
     id: event.conversation.conversation_id,
     participants: event.conversation.conversation.participant_data.map((participant: any) => participant.fallback_name),
     title: 'None',
-  }))
+  })))
   const chats = chatConversation.concat(chatEvent)
   return {
     data: chats,
