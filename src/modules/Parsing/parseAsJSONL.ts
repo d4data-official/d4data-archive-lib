74f972cfe7da50c3fa5b9c6367c729827c84c704
Pedro <pedrosf1000@gmail.com>
2021-04-05 17:41:33 +0200

    Add JSONLine parser

--- src/modules/Parsing/parseAsJSONL.ts
+++ src/modules/Parsing/parseAsJSONL.ts
@@ -1,3 +1,4 @@
+import readline from 'readline'
 import { PaginationOptions, ParsingOptions } from '../../types/Parsing'
 import Pipeline from '../../classes/Pipeline'
 
@@ -10,5 +11,33 @@ export default async function parseAsJSONL<T = any>(
   pipeline: Pipeline,
   options?: OptionsParseAsJSONL,
 ): Promise<Array<T>> {
-  return Promise.reject(new Error('Not implemented'))
+  const content: Array<T> = []
+  let i = 0
+  const stream = pipeline.run()
+  const rl = readline.createInterface({
+    input: stream,
+  });
+
+  rl.on('line', (line) => {
+    if (i < (options?.pagination?.offset || 0)) {
+      i += 1
+      return
+    }
+    if (i >= (options?.pagination?.offset ?? 0) + (options?.pagination?.items ?? Infinity)) {
+      stream.destroy()
+      rl.close()
+      return
+    }
+    content.push(JSON.parse(line))
+    i += 1
+  });
+
+  return new Promise <Array<T>>((resolve, reject) => {
+    rl.on('close', () => {
+      resolve(content)
+    })
+    stream.on('error', (e) => {
+      reject(e)
+    })
+  })
 }
