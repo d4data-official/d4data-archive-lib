3e872bb7732d70ca0814112120ef3437a5b9f8e1
valentin <valentinszabo01@gmail.com>
2021-04-07 21:32:24 +0900

    Change import

--- src/modules/Parsing/parseAsVCARD.ts
+++ src/modules/Parsing/parseAsVCARD.ts
@@ -1,3 +1,4 @@
+import vcf from 'vcf'
 import { ParsingOptions } from '../../types/Parsing'
 import Pipeline from '../../classes/Pipeline'
 
@@ -7,8 +8,6 @@ export type OptionsParseAsVCARD = ParsingOptions
  * Parse given Pipeline result stream as VCARD format
  */
 export default async function parseAsVCARD(pipeline: Pipeline, options?: OptionsParseAsVCARD): Promise<any> {
-  // eslint-disable-next-line global-require
-  const vCard = require('vcf')
   const fileDump = await pipeline.toString()
-  return vCard.parse(fileDump)
+  return vcf.parse(fileDump)
 }
