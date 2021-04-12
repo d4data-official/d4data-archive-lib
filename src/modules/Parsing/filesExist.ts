3564ea7fd2c6f737466d18a8f00706b4669c4672
Clovis de Villars <clovis.devillars@gmail.com>
2021-04-13 00:16:10 +0200

    fix(project): fix fs promises imports

--- src/modules/Parsing/filesExist.ts
+++ src/modules/Parsing/filesExist.ts
@@ -1,11 +1,10 @@
-import { access } from 'fs/promises';
-import { constants } from 'fs';
+import { promises as fsPromises, constants } from 'fs';
 
 /**
  * Returns boolean whether if given files exist
  */
 export default async function filesExist(dirPaths: Array<string>): Promise<boolean> {
-  return Promise.all(dirPaths.map((path) => access(path, constants.R_OK)))
+  return Promise.all(dirPaths.map((path) => fsPromises.access(path, constants.R_OK)))
     .then(() => true)
     .catch(() => false)
 }
