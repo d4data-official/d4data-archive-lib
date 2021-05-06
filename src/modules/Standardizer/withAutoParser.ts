faa075bd47b7371caeda83a0c86fec6efaf6ada9
Clovis de Villars <clovis.devillars@gmail.com>
2021-05-06 20:46:43 +0200

    feat(Standardizer): add helper to auto manage Parser in external getters

---
+++ src/modules/Standardizer/withAutoParser.ts
@@ -0,0 +1,23 @@
+import Getter from '../../types/standardizer/Getter'
+import Standardizer from '../../classes/Standardizer/Standardizer'
+import { GetterOptions } from '../../types/standardizer/Standardizer'
+import GetterReturn from '../../types/standardizer/GetterReturn'
+import Parser from '../../classes/Parser'
+
+export type WrappedGetter<T> = (this: Standardizer, parser: Parser, options?: GetterOptions) => Promise<T>
+
+export default function withAutoParser<T>(externalGetter: WrappedGetter<T>): Getter<T> {
+  return async function (this: Standardizer, options?: GetterOptions): GetterReturn<T> {
+    const parser = this.newParser()
+    const boundGetter = externalGetter.bind(this)
+
+    if (options?.parsingOptions) {
+      parser.mergeWithDefaultOptions(options.parsingOptions)
+    }
+
+    return {
+      data: await boundGetter(parser, options),
+      parsedFiles: parser.parsedFiles,
+    }
+  }
+}
