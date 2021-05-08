f49253def1237911ec744d772e9169ec33339586
Nathan Bonnot <nathan.bonnot@epitech.eu>
2021-05-08 14:40:38 +0200

    removed chatEvent

--- src/classes/Standardizer/plugins/Google/getters/getChats.ts
+++ src/classes/Standardizer/plugins/Google/getters/getChats.ts
@@ -5,17 +5,12 @@ const ACCOUNT_CHAT_FILE = 'Takeout/Hangouts/Hangouts.json'
 
 Google.prototype.getChats = async function getChats(options) {
   const rawChats = await this.parser.parseAsJSON(ACCOUNT_CHAT_FILE, options?.parsingOptions)
-  const chatConversation: Array<Chat> = await Promise.all(rawChats.conversations.map((conv: any) => ({
-    id: conv.conversation.conversation_id,
+  const chats: Array<Chat> = await Promise.all(rawChats.conversations.map((conv: any) => ({
+    id: conv.conversation.conversation_id.id,
     participants: conv.conversation.conversation.participant_data.map((participant: any) => participant.fallback_name),
     title: 'None',
   })))
-  const chatEvent: Array<Chat> = await Promise.all(rawChats.conversations.map((event: any) => ({
-    id: event.conversation.conversation_id,
-    participants: event.conversation.conversation.participant_data.map((participant: any) => participant.fallback_name),
-    title: 'None',
-  })))
-  const chats = chatConversation.concat(chatEvent)
+  console.log(chats)
   return {
     data: chats,
     parsedFiles: [ACCOUNT_CHAT_FILE],
