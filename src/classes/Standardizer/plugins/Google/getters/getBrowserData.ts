ca35d6b189c9cdf3890dd5696b7a219947b10631
Clovis de Villars <clovis.devillars@gmail.com>
2021-05-09 21:33:14 +0200

    fix(google-getBrowserData): handle encrypted browser data

--- src/classes/Standardizer/plugins/Google/getters/getBrowserData.ts
+++ src/classes/Standardizer/plugins/Google/getters/getBrowserData.ts
@@ -1,9 +1,9 @@
-import Google from '../Google';
-import { History, Extension, SavedForm, Theme, Preference } from '../../../../../types/schemas/BrowserData';
-import { BrowserData } from '../../../../../types/schemas';
+import Google from '../Google'
+import { Extension, History, Preference, SavedForm, Theme } from '../../../../../types/schemas/BrowserData'
+import { BrowserData } from '../../../../../types/schemas'
 
 function getStoreUrl(id: string) {
-  return id ? `https://chrome.google.com/webstore/detail/TEXT/${ id }` : undefined;
+  return id ? `https://chrome.google.com/webstore/detail/TEXT/${ id }` : undefined
 }
 
 const files = {
@@ -16,18 +16,38 @@ const files = {
 }
 
 interface GoogleSavedForm {
-  use_count: any; use_date: any; name_first: any[]; name_last: any[]; name_full: any[];
-  phone_home_whole_number: any[]; email_address: any[]; company_name: any; address_home_language_code: any;
-  address_home_city: any; address_home_country: any; address_home_line1: any; address_home_line2: any;
-  address_home_state: any; address_home_street_address: any; address_home_zip: any;
+  use_count: any;
+  use_date: any;
+  name_first: any[];
+  name_last: any[];
+  name_full: any[];
+  phone_home_whole_number: any[];
+  email_address: any[];
+  company_name: any;
+  address_home_language_code: any;
+  address_home_city: any;
+  address_home_country: any;
+  address_home_line1: any;
+  address_home_line2: any;
+  address_home_state: any;
+  address_home_street_address: any;
+  address_home_zip: any;
 }
 
 interface GoogleHistoryEntry {
-  time_usec: number; title: any; url: any; favicon_url: any; client_id: any;
+  time_usec: number;
+  title: any;
+  url: any;
+  favicon_url: any;
+  client_id: any;
 }
 
 interface GoogleExtension {
-  name: any; enabled: any; incognito_enabled: any; version: any; id: string;
+  name: any;
+  enabled: any;
+  incognito_enabled: any;
+  version: any;
+  id: string;
 }
 
 Google.prototype.getBrowserData = async function getBrowserData(options) {
@@ -48,9 +68,14 @@ Google.prototype.getBrowserData = async function getBrowserData(options) {
     extensions,
     searchEngines,
     syncSettings,
-  };
+  }
 
-  const browserData : BrowserData = {
+  // Check for encrypted browser data
+  if (rawBrowserData.extensions?.Extensions[0]?.Encrypted) {
+    return null
+  }
+
+  const browserData: BrowserData = {
     history: rawBrowserData.history?.map(
       (historyEntry: GoogleHistoryEntry) => ({
         datetime: new Date(historyEntry.time_usec as number / 1000),
@@ -94,7 +119,7 @@ Google.prototype.getBrowserData = async function getBrowserData(options) {
       name: rawBrowserData.syncSettings?.Themes[0]?.custom_theme_name,
       websiteUrl: getStoreUrl(rawBrowserData.syncSettings?.Themes[0]?.custom_theme_id),
     } as Theme,
-  };
+  }
 
   return {
     data: browserData,
