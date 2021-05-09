7a869bf5e8996cac8ea6bc5d0343547c8bd2b237
Jessy SOBREIRO <jessy.sobreiro@epitech.eu>
2021-05-10 00:23:43 +0200

    Fix bad typing

--- src/classes/Standardizer/plugins/Discord/getters/getSettings.ts
+++ src/classes/Standardizer/plugins/Discord/getters/getSettings.ts
@@ -6,7 +6,7 @@ const SETTINGS_FILE = 'account/user.json'
 Discord.prototype.getSettings = async function getSettings(options) {
   const settingsRaw = await this.parser.parseAsJSON(SETTINGS_FILE, options?.parsingOptions)
 
-  const settings : Array<Setting> = Object.keys(settingsRaw.settings).map((key) => ({
+  const settings : Array<Setting> = Object.keys(settingsRaw.settings).map((key) : Setting => ({
     name: key,
     value: settingsRaw.settings?.[key]?.toString(),
   }));
