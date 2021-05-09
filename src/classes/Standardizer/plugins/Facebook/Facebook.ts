57f65925bf0631cc640cb230e5747fba43802bd9
Alexandre Monier <alexandre.monierc@gmail.com>
2021-05-10 00:40:56 +0200

    Fixing preprocessor

--- src/classes/Standardizer/plugins/Facebook/Facebook.ts
+++ src/classes/Standardizer/plugins/Facebook/Facebook.ts
@@ -1,4 +1,6 @@
 import path from 'path'
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
 import Standardizer, { EXTERNAL_GETTERS_DIR } from '../../Standardizer'
 import Services from '../../../../types/Services'
 import { PipelineStep } from '../../../Pipeline'
@@ -7,14 +9,19 @@ export const preProcessors: Array<PipelineStep> = [
   // Pre-processor to fix bad string encoding in Facebook archive JSON files
   async function* utf8Translator(stream) {
     if (stream !== undefined) {
+      let content = '';
       for await (const chunk of stream) {
-        const chunkStr: string = chunk.toString();
-        const semiFixedStr: string = chunkStr.replaceAll(/\\u00([0-9a-f]{2})/g,
-          (_: any, group: string) => `%${ group.toUpperCase() }`)
-        const fixedStr = decodeURI(semiFixedStr)
-        const newChunk = Buffer.from(fixedStr, 'utf8');
-        yield newChunk
+        content += chunk.toString();
       }
+      const MAGIC_STRING: string = uuidv4();
+      const chunkStr: string = content.toString();
+      const encodedURI: string = chunkStr.replaceAll('%', MAGIC_STRING);
+      const semiFixedStr: string = encodedURI.replaceAll(/\\u00([0-9a-f]{2})/g,
+        (_: any, group: string) => `%${ group.toUpperCase() }`)
+      const fixedStr = semiFixedStr.replaceAll(/(?:%[A-Z0-9]{2})+/g, decodeURI)
+      // eslint-disable-next-line no-control-regex
+      const decodedStr = fixedStr.replaceAll(MAGIC_STRING, '%').replaceAll(/[\u0000-\u001F\u007F-\u009F]/g, '')
+      yield decodedStr
     }
   },
 ]
