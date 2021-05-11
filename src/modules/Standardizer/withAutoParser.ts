520e40673747f802261a4b84ea3468c8b0a1b9c0
Alexandre Monier <alexandre.monierc@gmail.com>
2021-05-11 02:39:47 +0200

    Even better withAutoParser

--- src/modules/Standardizer/withAutoParser.ts
+++ src/modules/Standardizer/withAutoParser.ts
@@ -6,20 +6,18 @@ import Parser from '../../classes/Parser'
 export type AutoParserGetter<T, TT extends unknown[]> = (
   this: Standardizer, ...args: [...TT, GetterOptions]) => GetterReturn<T>
 
-export type WrappedGetter<T, TT> = (
-  this: Standardizer, parser: Parser, options?: GetterOptions, ...args: Array<TT>) => Promise<T>
+export type WrappedGetter<T, TT extends unknown[]> = (
+  this: Standardizer, parser: Parser, ...args: [...TT, GetterOptions]) => Promise<T>
 
 export default function withAutoParser<T, TT extends unknown[]>(
   externalGetter: WrappedGetter<T, TT>,
 ): AutoParserGetter<T, TT> {
   return async function autoParser(this, ...args): GetterReturn<T> {
     const options: GetterOptions = args[args.length - 1] as GetterOptions
-    const slicedArgs = args.slice(0, -1) as unknown as TT[]
     const parser = this.newParser(options?.parsingOptions)
-    const boundGetter = externalGetter.bind(this)
 
     return {
-      data: await boundGetter(parser, options, ...slicedArgs),
+      data: await externalGetter.call(this, parser, ...args),
       parsedFiles: parser.parsedFiles,
     }
   }
