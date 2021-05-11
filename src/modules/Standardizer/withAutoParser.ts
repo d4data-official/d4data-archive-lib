2611dd3d8ef2cfdbc6c8bb6155be77ff2748c37d
Alexandre Monier <alexandre.monierc@gmail.com>
2021-05-11 02:24:44 +0200

    Adding type inference and value precision

--- src/modules/Standardizer/withAutoParser.ts
+++ src/modules/Standardizer/withAutoParser.ts
@@ -1,35 +1,26 @@
-import Getter from '../../types/standardizer/Getter'
 import Standardizer from '../../classes/Standardizer/Standardizer'
 import { GetterOptions } from '../../types/standardizer/Standardizer'
 import GetterReturn from '../../types/standardizer/GetterReturn'
 import Parser from '../../classes/Parser'
 
-export type WrappedGetter<T> = (this: Standardizer, parser: Parser, options?: GetterOptions) => Promise<T>
+export type AutoParserGetter<T, TT extends unknown[]> = (
+  this: Standardizer, ...args: [...TT, GetterOptions]) => GetterReturn<T>
 
-function withAutoParser<T>(options: GetterOptions, externalGetter: WrappedGetter<T>): Getter<T>;
-function withAutoParser<T>(externalGetter: WrappedGetter<T>): Getter<T>;
-function withAutoParser<T>(
-  optionsOrExternalGetter: GetterOptions | WrappedGetter<T>,
-  externalGetter?: WrappedGetter<T>,
-): Getter<T> {
-  return async function autoParser(this: Standardizer, options?: GetterOptions): GetterReturn<T> {
-    let effectiveOptions: GetterOptions | undefined;
-    let effectiveExternalGetter: WrappedGetter<T>;
-    if (externalGetter instanceof Function) {
-      effectiveOptions = optionsOrExternalGetter as GetterOptions;
-      effectiveExternalGetter = externalGetter
-    } else {
-      effectiveOptions = options;
-      effectiveExternalGetter = optionsOrExternalGetter as WrappedGetter<T>;
-    }
-    const parser = this.newParser(effectiveOptions?.parsingOptions)
-    const boundGetter = effectiveExternalGetter.bind(this)
+export type WrappedGetter<T, TT> = (
+  this: Standardizer, parser: Parser, options?: GetterOptions, ...args: Array<TT>) => Promise<T>
+
+export default function withAutoParser<T, TT extends unknown[]>(
+  externalGetter: WrappedGetter<T, TT>,
+): AutoParserGetter<T, TT> {
+  return async function autoParser(this, ...args): GetterReturn<T> {
+    const options: GetterOptions = args[args.length - 1] as GetterOptions
+    const slicedArgs = args.slice(0, -1) as unknown as TT[]
+    const parser = this.newParser(options?.parsingOptions)
+    const boundGetter = externalGetter.bind(this)
 
     return {
-      data: await boundGetter(parser, options),
+      data: await boundGetter(parser, options, ...slicedArgs),
       parsedFiles: parser.parsedFiles,
     }
   }
 }
-
-export default withAutoParser
