f79fedbe156ee458a92fcb22b3062802332fa5d4
Jessy SOBREIRO <jessy.sobreiro@epitech.eu>
2021-05-17 12:58:22 +0200

    Fix Valentin's parseAsMBOX function

--- src/modules/Parsing/parseAsMBOX.ts
+++ src/modules/Parsing/parseAsMBOX.ts
@@ -1,5 +1,4 @@
 import mboxParser, { ParsedMail } from 'mbox-parser'
-import { AddressObject, EmailAddress } from 'mailparser'
 import { PaginationOptions, ParsingOptions } from '../../types/Parsing'
 import Pipeline from '../../classes/Pipeline'
 import { Mail } from '../../types/schemas'
@@ -10,42 +9,22 @@ export type OptionsParseAsMBOX = ParsingOptions & PaginationOptions
  * Parse given Pipeline result stream as MBOX format
  */
 export default async function parseAsMBOX(pipeline: Pipeline, options?: OptionsParseAsMBOX): Promise<Array<any>> {
-  return mboxParser(pipeline.run(), {
+  const mboxFile = await mboxParser(pipeline.run(), {
     pageSize: options?.pagination?.items ?? 50,
     pageNumber: 1, // tmp
-  }).then((mails: Array<ParsedMail>) => {
-    const address: Array<string> = []
-    for (let x: number = 0; x < mails.length; x += 1) {
-      const tmp: ParsedMail = mails[x]
-      if (tmp.to) {
-        if (Array.isArray(tmp.to)) {
-          for (let y: number = 0; y < tmp.to.length; y += 1) {
-            const tmpTo: AddressObject = tmp.to[y]
-            for (let z: number = 0; z < tmpTo.value.length; z += 1) {
-              const tmpAddress = tmpTo.value[z].address
-              if (tmpAddress !== undefined) {
-                address.push(tmpAddress)
-              }
-            }
-          }
-        } else {
-          for (let z: number = 0; z < tmp.to.value.length; z += 1) {
-            const tmpAddress = tmp.to.value[z].address
-            if (tmpAddress !== undefined) {
-              address.push(tmpAddress)
-            }
-          }
-        }
-      }
-    }
-    const formatedMails: Array<Mail> = mails.map((mail: ParsedMail) => ({
+  })
+
+  return mboxFile.map((mail: ParsedMail) => {
+    const toAddresses = Array.isArray(mail.to)
+      ? mail.to.map(t => t?.text)
+      : [mail.to?.text ?? 'Unknown']
+    return ({
       from: mail.from?.value[0].address ?? '',
-      to: address,
+      to: toAddresses ?? ['Unknown'],
       date: mail.date,
       subject: mail.subject,
-      content: mail.text,
+      content: mail.html ?? mail.textAsHtml ?? mail.text ?? 'Empty mail',
       attachments: [], // tmp
-    }));
-    return formatedMails
-  });
+    })
+  })
 }
