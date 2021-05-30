cbac9aa624b011e2210a4b69b2d4b2b9ff2cf7c0
Clovis de Villars <clovis.devillars@gmail.com>
2021-05-30 13:13:35 +0200

    feat(Standardizer): add Gravatar plugin

---
+++ src/classes/Standardizer/plugins/Gravatar/Gravatar.ts
@@ -0,0 +1,19 @@
+import path from 'path'
+import Standardizer, { EXTERNAL_GETTERS_DIR } from '../../Standardizer'
+import Services from '../../../../types/Services'
+
+export default class Gravatar extends Standardizer {
+  get service(): Services {
+    return Services.GRAVATAR
+  }
+
+  get subServices(): Array<Services> {
+    return []
+  }
+
+  get subStandardizers(): Array<Standardizer> {
+    return []
+  }
+}
+
+Standardizer.importExternalGettersSync(path.resolve(__dirname, EXTERNAL_GETTERS_DIR))
