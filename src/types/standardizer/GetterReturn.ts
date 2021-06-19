83c7c9e5a513e97ef037341fa2a2485b0fce641d
Clovis de Villars <clovis.devillars@gmail.com>
2021-06-19 19:20:43 +0200

    feat(types): add statistics property in getter return type

--- src/types/standardizer/GetterReturn.ts
+++ src/types/standardizer/GetterReturn.ts
@@ -1,4 +1,10 @@
-export type GetterData<T> = { data: T, parsedFiles: Array<string> } | null
+import Statistic from '../schemas/Statistic'
+
+export type GetterData<T> = {
+  data: T,
+  parsedFiles: Array<string>,
+  statistics?: Array<Statistic>,
+} | null
 
 type GetterReturn<T> = Promise<GetterData<T>>
 
