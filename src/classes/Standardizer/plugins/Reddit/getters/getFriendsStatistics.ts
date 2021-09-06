14616f67822f665b1c73b21e2cc7216d9e257015
Nathan Bonnot <nathan.bonnot@epitech.eu>
2021-09-06 13:56:15 +0200

    Statitistique Friends

---
+++ src/classes/Standardizer/plugins/Reddit/getters/getFriendsStatistics.ts
@@ -0,0 +1,43 @@
+import Reddit from '../Reddit'
+import { StatisticType } from '../../../../../types/schemas/Statistic'
+
+Reddit.prototype.getFriendsStatistics = async function getFriendsStatistics() {
+  let Nyear = 1
+  const FriendData = await this.getFriends({
+    parsingOptions: {
+      pagination: {
+        offset: 0,
+        items: Infinity,
+      },
+    },
+  })
+
+  if (!FriendData) {
+    return null
+  }
+  const lastFriend = Date.now() - new Date(Math.max(...FriendData.data.map(x => x.date!.getTime()))).getTime()
+  FriendData.data.forEach((entry) => {
+    const end = new Date(new Date().setFullYear(new Date().getFullYear() + Nyear))
+    for (Nyear; entry.date! < end; Nyear += 1) {
+      Nyear += 1
+    }
+  })
+
+  console.log((FriendData.data.length / Nyear))
+  console.log(lastFriend)
+  return {
+    statistics: [
+      {
+        type: StatisticType.NUMBER,
+        value: (FriendData.data.length / Nyear),
+        name: 'Friend over Time',
+      },
+      {
+        type: StatisticType.DURATION,
+        value: lastFriend,
+        name: 'Time since Last Friends added',
+      },
+    ],
+    parsedFiles: FriendData?.parsedFiles ?? [],
+  }
+}
