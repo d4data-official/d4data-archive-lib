8df5782640015a9a8e5833ac454d4221c50ee855
Nathan Bonnot <nathan.bonnot@epitech.eu>
2021-09-08 11:13:27 +0200

    Statistics Comments

---
+++ src/classes/Standardizer/plugins/Facebook/getters/getCommentsStatistics.ts
@@ -0,0 +1,37 @@
+import Facebook from '../Facebook'
+import { StatisticType } from '../../../../../types/schemas/Statistic'
+
+Facebook.prototype.getCommentsStatistics = async function getCommentsStatistics() {
+  let Nyear = 1
+  const commentsData = await this.getComments({
+    parsingOptions: {
+      pagination: {
+        offset: 0,
+        items: Infinity,
+      },
+    },
+  })
+
+  if (!commentsData) {
+    return null
+  }
+  commentsData.data.forEach((entry) => {
+    const end = new Date(new Date().setFullYear(new Date().getFullYear() + Nyear))
+    for (Nyear; entry.creationDate < end; Nyear += 1) {
+      Nyear += 1
+    }
+  })
+
+  console.log((commentsData.data.length / Nyear))
+  console.log(lastcomments)
+  return {
+    statistics: [
+      {
+        type: StatisticType.NUMBER,
+        value: (commentsData.data.length / Nyear),
+        name: 'comments over Time',
+      },
+    ],
+    parsedFiles: commentsData?.parsedFiles ?? [],
+  }
+}
