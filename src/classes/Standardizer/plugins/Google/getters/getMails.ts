3dbf69f27ff64fb5e6b7cf449d9d07da16117e57
Clovis de Villars <clovis.devillars@gmail.com>
2021-09-10 23:18:52 +0200

    fix(Google-getters): fix getters for new ParserReturn type

--- src/classes/Standardizer/plugins/Google/getters/getMails.ts
+++ src/classes/Standardizer/plugins/Google/getters/getMails.ts
@@ -1,13 +1,16 @@
 import Google from '../Google'
-import { Mail } from '../../../../../types/schemas'
 import withAutoParser from '../../../../../modules/Standardizer/withAutoParser'
 
 const CONNECTIONS_FOLDER = 'Takeout/Mail'
 
 Google.prototype.getMails = withAutoParser(async parser => {
-  const files = await parser.listFiles(CONNECTIONS_FOLDER, { extensionWhitelist: ['mbox'] });
+  const files = await parser.listFiles(CONNECTIONS_FOLDER, { extensionWhitelist: ['mbox'] })
 
-  const mails = await Promise.all<Array<Mail>>(files.map(async (file: string) => parser.parseAsMBOX(file)))
+  const mails = await Promise.all(
+    files.map(async (file: string) => parser.parseAsMBOX(file)),
+  )
 
-  return mails.flat()
+  return mails
+    .map(({ data }) => data)
+    .flat()
 })
