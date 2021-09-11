7c26842a7918d8704f8d3e42af12658eb4a85977
valentin <valentinszabo01@gmail.com>
2021-09-12 00:26:52 +0900

    fix coding style

--- src/classes/Standardizer/plugins/Facebook/getters/getContactsStatistics.ts
+++ src/classes/Standardizer/plugins/Facebook/getters/getContactsStatistics.ts
@@ -3,48 +3,43 @@ import { StatisticType } from '../../../../../types/schemas/Statistic'
 
 // 3 noms et le nombre d'occurence
 
-function getFirstName(fullName : string)
-{
-  return fullName.split(' ')[0]
+function getFirstName(fullName : string) {
+  return fullName.split(' ')[0];
 }
 
 Facebook.prototype.getContactsStatistics = async function getContactsStatistics() {
-
-    const contactsData = await this.getContacts({
-      parsingOptions: {
-        pagination: {
-          offset: 0,
-          items: Infinity,
-        },
+  const contactsData = await this.getContacts({
+    parsingOptions: {
+      pagination: {
+        offset: 0,
+        items: Infinity,
       },
-    })
-  
-    if (!contactsData)
-      return null
+    },
+  })
 
-    var contactMap = new Map()
-    contactsData.data.forEach((entry) => {
-        const firstName : string = entry.displayName === undefined ? "" : getFirstName(entry.displayName);
-        if(contactMap.has(firstName))
-            contactMap.set(firstName, contactMap.get(firstName) +1);
-        else
-            contactMap.set(firstName, 1);
-    })
+  if (!contactsData) { return null }
 
-    const contactMapSorted = new Map([...contactMap.entries()].sort((a, b) => b[1] - a[1]));
+  const contactMap = new Map()
+  contactsData.data.forEach((entry) => {
+    const firstName : string = entry.displayName === undefined ? '' : getFirstName(entry.displayName);
+    if (contactMap.has(firstName)) contactMap.set(firstName, contactMap.get(firstName) + 1);
+    else contactMap.set(firstName, 1);
+  })
 
-    let keys = Array.from(contactMapSorted.keys()).slice(0, 3);
-    let values = Array.from(contactMapSorted.values()).slice(0, 3);
-    const mapContactsLimited = keys.map((el, i) => ({ firstName: el, value: values[i] }));
+  const contactMapSorted = new Map([...contactMap.entries()].sort((a, b) => b[1] - a[1]));
 
-    return {
-      statistics: [
-        {
-          type: StatisticType.RANKING,
-          value: mapContactsLimited,
-          name: 'Contacts with the same first name.',
-        },
-      ],
-      parsedFiles: contactsData?.parsedFiles ?? [],
-    }
+  const keys = Array.from(contactMapSorted.keys()).slice(0, 3);
+  const values = Array.from(contactMapSorted.values()).slice(0, 3);
+  const mapContactsLimited = keys.map((el, i) => ({ firstName: el, value: values[i] }));
+
+  return {
+    statistics: [
+      {
+        type: StatisticType.RANKING,
+        value: mapContactsLimited,
+        name: 'Contacts with the same first name.',
+      },
+    ],
+    parsedFiles: contactsData?.parsedFiles ?? [],
+  }
 }
