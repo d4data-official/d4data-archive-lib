6110c1e07d0302d26b36d72d964145b5418c9f53
Clovis de Villars <clovis.devillars@gmail.com>
2021-10-20 14:15:28 +0200

    fix(getEvents): catch no activity files case

--- src/classes/Standardizer/plugins/Discord/getters/getEvents.ts
+++ src/classes/Standardizer/plugins/Discord/getters/getEvents.ts
@@ -9,8 +9,22 @@ export type DiscordEvent = Record<string, any> & {
 
 Discord.prototype.getEvents = async function (options) {
   let parsedEventCounter = 0
-  const { items, offset } = options?.parsingOptions?.pagination ?? this.parser.defaultOptions.pagination
+
+  const {
+    items,
+    offset,
+  } = options?.parsingOptions?.pagination ?? this.parser.defaultOptions.pagination
+
   const filePathList = await this.parser.findFiles(/(analytics|modeling|reporting|tns)\/events-.*\.json$/, 'activity/')
+    .catch(error => {
+      console.error(error)
+      return null
+    })
+
+  if (filePathList === null) {
+    return null
+  }
+
   const rawEvents: Array<Array<DiscordEvent>> = []
 
   for await (const filePath of filePathList) {
@@ -29,12 +43,13 @@ Discord.prototype.getEvents = async function (options) {
     rawEvents.push(parsedEvents)
   }
 
-  const events: Array<Event> = rawEvents.flat().map((rawEvent): Event => ({
-    type: rawEvent.event_type,
-    // Slice to delete duplicate double quotes (ex: "\"2019-06-06T10:21:19.103Z\"")
-    date: new Date(rawEvent.timestamp.slice(1, -1)),
-    extra: rawEvent,
-  }))
+  const events: Array<Event> = rawEvents.flat()
+    .map((rawEvent): Event => ({
+      type: rawEvent.event_type,
+      // Slice to delete duplicate double quotes (ex: "\"2019-06-06T10:21:19.103Z\"")
+      date: new Date(rawEvent.timestamp.slice(1, -1)),
+      extra: rawEvent,
+    }))
 
   return {
     data: events,
